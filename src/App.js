d0df3870e89c487ade1ffe07cf7aeaeaa365afd7
Matjaz Vidmar <wei.majiang@gmail.com>
2022-09-12 21:37:40 +0200

    Added features

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import randomcolor from 'randomcolor';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
-function App1() {
-  const [color, setColor] = setState();
+function App() {
+  const [color, setColor] = useState(
+    randomColor({
+      luminosity: 'random',
+      hue: 'random',
+    }),
+  );
+
   return (
     <div>
       <h1>Random color generator</h1>
-    </div>
-    <div>
+
+      <div
+        style={{
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: color,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <input
+          value={'Generate color:' + color}
+          onClick={(event) => {
+            setColor(event.currentTarget.value);
+          }}
+        ></input>
         {color}
       </div>
-        <button
+
+      <button
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingVertical: 12,
+          paddingHorizontal: 32,
+          borderRadius: 4,
+          elevation: 3,
+          backgroundColor: 'white',
+        }}
         onClick={() => {
-          const newColor = (randomColor().color);
-          setColor(newColor)
-        }
-        }></button>
+          const newColor = randomColor({
+            luminosity: 'random',
+            hue: 'random',
+          });
+          setColor(newColor);
+        }}
+      >
+        Generate
+      </button>
+    </div>
   );
 }
 
-export default App1;
+export default App;
